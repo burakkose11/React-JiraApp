08b902a2825a9833f107f7bb818708903c627d0a
Burak Kose <burakkose011@icloud.com>
2023-03-11 14:43:37 +0300

    task create olusturuldu

---
+++ src/components/TaskCreate.js
@@ -0,0 +1,16 @@
+function TaskCreate() {
+  return (
+    <div>
+      <h3>Lütfen Task Ekleyiniz</h3>
+      <form>
+        <label>Başlık</label>
+        <input />
+        <label>Task Giriniz</label>
+        <textarea>Oluştur</textarea>
+        <button>Oluştur</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskCreate;
